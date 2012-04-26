4dfaa470257255fe50f5bf5a039fc605aef8cdce
inzane <curtismfoster@gmail.com>
2012-04-25 22:44:52 -0500

    added wallet check and provided format to create new bountied character easily

--- app/models/eve_api_checker.rb
+++ app/models/eve_api_checker.rb
@@ -1,32 +1,50 @@
-class EveApiChecker
+class EveApiChecker 
   
-  @@last_request_time = Time
+  @@last_request_time = nil
   
   #check corporation wallet journal for all refTypeID's of 10(donations) and dated after last donation recieved.
-  def self.check_corp_journal_for_new_donations
+  def self.check_corp_journal_for_new_donations(test_time=nil)
     acct = eve_api
     begin
-      entries = acct.walletjournal.entries
+      wallet = acct.walletjournal
+      entries = wallet.entries
     rescue EAAL::Exception.EveAPIException(500)
       return false
     rescue EAAL::Exception.EveAPIException(904)
       return false
     end
-    @@last_request_time ||= acct.request_time.to_time #Set @@last_request_time in case this is the initial launch, which is would be nil.
-    all_transfers = entries.collect {
-      |e| e.reason.gsub(/[DECS:\s]/, "") if e.refTypeID.to_i == 10 && e.date.to_time > @@last_request_time
+    @@last_request_time ||= wallet.request_time.to_time #Set @@last_request_time in case this is the initial launch, which is would be nil.
+    outlaw_names = entries.collect {
+      |e| [e.reason.gsub(/[DECS:\n\r]/, ""), e.balance] if e.refTypeID.to_i == 10 && e.date.to_time > (test_time || @@last_request_time)
     }
-    log_api_access_time(acct.request_time.to_time)
-    return all_transfers
+  return outlaw_names unless test_time.nil?
+    outlaw_names.delete_if {|t| t == nil}
+    outlaw_names.each{|d| d[0].gsub!(/^\s/, "")}
+    log_api_access_time(wallet.request_time.to_time)
+    get_character_id(acct, outlaw_names) if !outlaw_names.empty?
   end
   
-  private
   def self.eve_api
     EAAL::API.new(CORP_USER_ID, CORP_VCODE, "corp")
   end
   
-  def log_api_access_time(time=nil)
+  def self.log_api_access_time(time=nil)
     time ? @@last_request_time = time : @@last_request_time 
+  end
+  
+  def self.get_character_id(acct, outlaw_names)
+    acct.scope = "eve"
+    begin
+      temp_coll = acct.characterID(:names => outlaw_names.join(',')).characters
+    rescue
+      return false
+    end
+    combine = temp_coll.collect{|t| [t.name,t.characterID]}.zip(outlaw_names.collect{|o| o[1]}).each{|t| t.flatten!})
+    #Output=> [["ACID BURNN", "648536349", "20000000.00"], ["Zer0 kooL", "1436418183", "10000000.00"]]
+    # [[NAME,CHARACTERID,BOUNTY PLACED]]
+    combine.each do |target|
+      WantedToon.make_wanted_toon(target[0],target[1],target[2])
+    end
   end
   
 end
--- app/models/wanted_toon.rb
+++ app/models/wanted_toon.rb
@@ -4,4 +4,8 @@ class WantedToon < ActiveRecord::Base
 #   Remember this to gsub the EAAL::API::CORP::WALLETJOURNAL result  s.entries.first.reason.gsub(/[DECS:\s]/, "")
 #   RefType 10 = Player Donation <= is what i am looking for only
 #   s.request_time.to_time = Keep an eye on this to reference next transaction to read.
+  
+  def make_wanted_toon(name,characterid,bounty)
+    
+  end
 end
