3f79594886c7b52475942a53565335cdf95abd43
cmfoster <curtismfoster@gmail.com>
2012-05-12 23:27:15 -0500

    styled ehl banner

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
   def index
     @wanted_list = WantedToon.active_bounties
     @eden_heros = EdenHero.top_earning_order
+    @total_bounty_pool = WantedToon.all.collect{|toon| toon.bounty}.sum
   end
   
 end
